1bb9175e80c48b56dfb8afd5eb8fe46ba145c4b7
milleand001 <milleand001@warren.k12.in.us>
2024-08-06 11:47:17 -0400

    added certain items

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,43 @@
+import java.awt.AWTEventMulticaster.add
+
 fun main() {
-    println("Hello World")
+    println("New chat message from a friend"}
 }
+fun string() {
+    var discountPercentage: Int = 0
+    var offer: String = "Sale - Up to"
+    val item = "Google Chromecast"
+    discountPercentage = 20
+    offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+    fun string() {
+        val numberOfAdults = "20"
+        val numberOfKids = "30"
+        val total = numberOfAdults + numberOfKids
+        println("The total party size is: $total")
+    }
+    fun string() {
+        val baseSalary = 5000
+        val bonusAmount = 1000
+        val totalSalary = "$baseSalary + $bonusAmount"
+        println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
+    }
+    fun string() {
+        val firstNumber = 10
+        val secondNumber = 5
+
+        var result = ""
+        println("$firstNumber + $secondNumber = $result")
+    }
+    fun string() {
+        val firstNumber = 10
+        val secondNumber = 5
+        val thirdNumber = 8
+
+        val result = add(firstNumber, secondNumber)
+        val anotherResult = add(firstNumber, thirdNumber)
+
+        println("$firstNumber + $secondNumber = $result")
+        println("$firstNumber + $thirdNumber = $anotherResult")
+    }
+
+// Define add() function below this line
