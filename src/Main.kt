89f0ed07b802c98b283545231f6b58930a3faf81
milleand001 <milleand001@warren.k12.in.us>
2024-08-08 11:23:39 -0400

    reverted and restarted to get some problems done.

--- src/Main.kt
+++ src/Main.kt
@@ -1,44 +1,56 @@
-import java.awt.AWTEventMulticaster.add
+
+
+
+/* Problem 2
+fun main() {
+    println("Use the val keyword when the value doesn't change. ")
+    println("Use the var keyword when the value can change.")
+    println("When you define a function, you define the parameters that can be passed to it. ")
+    println("When you call a function, you pass arguments for the parameters.")
+
+}
+*/
+
+/* Problem 3
+fun main() {
+    println("New chat message from a friend")
+}
+*/
 
 fun main() {
-    println("New chat message from a friend"}
-}
-fun main() {
     var discountPercentage: Int = 0
-    var offer: String = "Sale - Up to"
+    var offer: String = ""
     val item = "Google Chromecast"
     discountPercentage = 20
     offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
 
-    fun main() {
-        val numberOfAdults = "20"
-        val numberOfKids = "30"
-        val total = numberOfAdults + numberOfKids
-        println("The total party size is: $total")
-    }
-    fun main() {
-        val baseSalary = 5000
-        val bonusAmount = 1000
-        val totalSalary = "$baseSalary + $bonusAmount"
-        println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
-    }
-    fun main() {
-        val firstNumber = 10
-        val secondNumber = 5
+    println(offer)
+}
 
-        var result = ""
-        println("$firstNumber + $secondNumber = $result")
-    }
-    fun main() {
-        val firstNumber = 10
-        val secondNumber = 5
-        val thirdNumber = 8
 
-        val result = add(firstNumber, secondNumber)
-        val anotherResult = add(firstNumber, thirdNumber)
+/*
+fun main() {
+    val numberOfAdults = 20
+    val numberOfKids = 30
+    val total = numberOfAdults + numberOfKids
+    println("The total party size is: $total")
+}
+ */
 
-        println("$firstNumber + $secondNumber = $result")
-        println("$firstNumber + $thirdNumber = $anotherResult")
-    }
+/*
+fun main() {
+    val baseSalary = 5000
+    val bonusAmount = 1000
+    val totalSalary = baseSalary + bonusAmount
+    println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
+}
+ */
+/*
+fun main() {
+    val firstNumber = 10
+    val secondNumber = 5
+    val result = firstNumber + secondNumber
+    println("$firstNumber + $secondNumber = $result")
+}
+ */
 
-// Define add() function below this line
